640097b3c1cdab681cbbb098b467aa3c3a24d8fc
migtorres <miguel.torres@unep-wcmc.org>
2013-06-06 18:38:19 +0100

    Added rails configuration

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+Tridb::Application.config.secret_token = '949f782337ecf77bf891bded920566f6c440d6076c38bfdc4c6bfe04c5b8a12ddf8b34a9a7b647db849dbfbb6a57961e3b553326b9416d3a45b8a14c100b2316'
